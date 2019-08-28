54f00a936d95c55a25f2f8a2b65802517cf0a2e6
nedelec <nedelec@embl.de>
2019-08-28 09:40:07 +0100

    new normalize(real)

--- src/math/vector.h
+++ src/math/vector.h
@@ -17,11 +17,17 @@ typedef Vector1 Vector;
 typedef real    Torque;
 const   Torque  nullTorque(0);
 
+/// helper function to normalize a 'Torque'
+inline real normalize(Torque x) { return std::copysign(1.0, x); }
+
 #elif ( DIM == 2 )
 
 typedef Vector2 Vector;
 typedef real    Torque;
 const   Torque  nullTorque(0);
+
+/// helper function to normalize a 'Torque'
+inline real normalize(Torque x) { return std::copysign(1.0, x); }
 
 #elif ( DIM == 3 )
 
