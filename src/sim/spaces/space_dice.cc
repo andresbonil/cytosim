7e6f9e00843e954359ab574ce2f84fba74ce9d92
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2020-06-24 08:24:13 +0200

    fixed bug in reading SpaceDice

--- src/sim/spaces/space_dice.cc
+++ src/sim/spaces/space_dice.cc
@@ -162,7 +162,7 @@ void SpaceDice::setLengths(const real len[])
     length_[0] = len[0];
     length_[1] = len[1];
     length_[2] = len[2];
-    radius_ = len[4];
+    radius_ = len[3];
     update();
 }
 
