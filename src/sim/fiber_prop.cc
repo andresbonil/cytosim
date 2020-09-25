ade98140cd0cae1bfa35b5c0184cf2fb724e7478
francois.nedelec@slcu.cam.ac.uk <francois.nedelec@slcu.cam.ac.uk>
2020-09-25 13:05:12 +0200

    fixed examples

--- src/sim/fiber_prop.cc
+++ src/sim/fiber_prop.cc
@@ -449,7 +449,7 @@ void FiberProp::complete(Simul const& sim)
         if ( lattice_unit <= 0 )
             throw InvalidParameter("fiber:lattice_unit (known as fiber:lattice[1]) must be specified and > 0");
 #else
-        throw InvalidParameter("cytosim cannot run with fiber:lattice");
+        throw InvalidParameter("Cytosim was not compile to handle fiber:lattice");
 #endif
     }
 
