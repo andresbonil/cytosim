7hekaka/newcytosim

System's tension calculated correctly

src/sim/meca.cc
@@ -133,8 +133,7 @@ void Meca::allocate(size_t alc)
         allocate_vector(alc, vRHS, 1);
         allocate_vector(alc, vFOR, 1);
         allocate_vector(alc, vTMP, 0);
-        
-        //std::clog << "Meca::allocate(" << allocated_ << ") " << vFOR << '\n';
+        //std::clog << "Meca::allocate(" << allocated_ << ")\n";
     }
 }
 
@@ -1344,34 +1343,20 @@ void Meca::prepareMatrices()
  Calculates forces due to external links, without adding Thermal motion,
  and also excluding bending elasticity of Fibers.
  
- This also sets the Lagrange multipliers for the Fiber.
+ Mecable::getForces will also sets the Lagrange multipliers for the Fiber.
  
- The function will not change the position of the Mecables.
+ The function will not change the positions of any Mecable.
  */
 void Meca::computeForces()
 {
     prepareMatrices();
     
-    // calculate forces into vFOR and vTMP
+    // vFOR <- external forces
     calculateForces(vPTS, vBAS, vFOR);
-    copy_real(dimension(), vFOR, vTMP);
 
-    // add rigidity, and calculate Lagrange Multipliers:
     for ( Mecable * mec : mecables )
     {
-        real * fff = vFOR + DIM * mec->matIndex();
-        real * ttt = vTMP + DIM * mec->matIndex();
-        
-        // add bending rigidity:
-#if SEPARATE_RIGIDITY_TERMS
-        real * xxx = vPTS + DIM * mec->matIndex();
-        mec->addRigidity(xxx, ttt);
-#endif
-        // calculate Lagrange multipliers for Fibers (irrelevant for other object)
-        mec->projectForces(ttt, ttt);
-        mec->storeTensions(ttt);
-        // register force (all objects)
-        mec->getForces(fff);
+        mec->getForces(vFOR+DIM*mec->matIndex());
     }
 }
 
@@ -1379,8 +1364,8 @@ void Meca::computeForces()
 /**
 This preforms:
  
-    vFOR <- vFOR + Noise
-    vRHS <- time_step * P * vFOR:
+    fff <- fff + Brownian
+    rhs <- time_step * P * fff:
  
  Also prepare Projection diff is requested
 
@@ -1393,15 +1378,16 @@ real brownian1(Mecable* mec, real const* rnd, real alpha, real* fff, real beta, 
     // Calculate the right-hand-side of the system in vRHS:
     mec->projectForces(fff, rhs);
     
-    // rhs <- beta * rhs, resulting in time_step * P * vFOR:
+    // rhs <- beta * rhs, resulting in time_step * P * fff:
     blas::xscal(DIM*mec->nbPoints(), beta*mec->leftoverMobility(), rhs, 1);
 
     /*
-     In this case, `fff` contains the true force in each vertex of the system
-     and the Lagrange multipliers will represent the tension in the filaments
+     At this stage, `fff` contains the external forces in each vertex but also
+     internal force such as bending elasticity terms, and the Lagrange multipliers
+     do not represent the true tension in the filaments.
+     Hence we do not call 'computeTensions(fff)' here
      */
-    mec->storeTensions(fff);
-    
+
 #if ADD_PROJECTION_DIFF
     mec->makeProjectionDiff(fff);
 #endif
@@ -1425,27 +1411,35 @@ real brownian1(Mecable* mec, real const* rnd, real alpha, real* fff, real beta, 
  
  where M is a matrix and B is a vector, leading to:
  
-     ( I - time_step * P * M ) ( Xnew - Xold ) = time_step * P * F + Noise
+     ( I - time_step * P * M ) ( Xnew - Xold ) = time_step * P * Force + Noise
  
  where:
  
-     F = M * Xold + B
+     Force = M * Xold + B
  
      Noise = std::sqrt(2*kT*time_step*mobility) * Gaussian(0,1)
  
  Implicit integration ensures numerical stability. In the code,
- the matrix M is decomposed as:
+ the sparse matrix M is decomposed as:
  
-     M = mB + mC + the rigidity terms of Mecables
+     M = mB + mC + Rigidity_of_Mecables
  
- and the vectors are:
+ Where mB is isotropic: it applies similarly in the X, Y and Z subspaces, and
+ has no crossterms between X and Y or X and Z or Y ans Z subspaces.
+ All crossterms go into mC.
+ The terms should already be set:
+
+     'mB', 'mC' and 'B=vBAS' are set in Meca::setAllInteractions()
+     'vPTS = Xold' is set from Mecables' points in Meca::prepare()
+     
+ The outline of the calculation is:
  
-     'vPTS' is Xold
-     'vBAS' is B
-     'vRND' is Noise, a vector of calibrated Brownian terms
-     'vFOR' is the force ( M * Xold + B ) and then ( M * Xnew + B )
-     'vRHS' is the right-hand-side of the system (time_step * P * F + vRND)
-     'vSOL' is `Xnew - Xold`, the solution to the system
+     'vRND' <- calibrated Gaussian random terms ~N(0,1)
+     'vFOR' <- force 'M * Xold + B'
+     'vRHS' <- time_step * P * F + vRND (right-hand-side of the system)
+     'vSOL' <- solution to the linear system of equations is calculated
+     'vSOL' <- 'Xnew = Xold + vSOL'
+     'vFOR' <- force 'M * Xnew + B'
  
  */
 size_t Meca::solve(SimulProp const* prop, const unsigned precond)
@@ -1455,7 +1449,7 @@ size_t Meca::solve(SimulProp const* prop, const unsigned precond)
 
     prepareMatrices();
     
-    // calculate forces before constraints in vFOR:
+    // calculate external forces in vFOR:
     calculateForces(vPTS, vBAS, vFOR);
     
 #if SEPARATE_RIGIDITY_TERMS
@@ -1687,16 +1681,24 @@ size_t Meca::solve(SimulProp const* prop, const unsigned precond)
     
     const auto total = ( __rdtsc() - start ) >> 10;
 
-    //add the solution of the system (=dPTS) to the points coordinates
+    //add the solution (the displacement) to update the Mecable's vertices
     blas::add(dimension(), vSOL, vPTS);
 
     /*
-     Re-calculate forces with the new coordinates, excluding bending elasticity
-     and Brownian terms on the vertices.
-     In this way the result returned to the fibers does not sum-up to zero,
-     and is appropriate for example to calculate the effect of force on assembly.
+     Re-calculate forces with the new coordinates, excluding bending elasticity.
+     In this way the forces returned to the fibers do not sum-up to zero, and
+     are appropriate for example to calculate the effect of force on assembly.
      */
-    //calculateForces(vPTS, vBAS, vFOR);
+    calculateForces(vPTS, vBAS, vFOR);
+    
+    // add Brownian terms:
+    for ( Mecable * mec : mecables )
+    {
+        const size_t inx = DIM * mec->matIndex();
+        mec->addBrownianForces(vRND+inx, alpha, vFOR+inx);
+        //fprintf(stderr, "\n  "); VecPrint::print(stderr, DIM*mec->nbPoints(), vFOR+inx, 2, DIM);
+    }
+
     ready_ = 1;
 
     // report on the matrix type and size, sparsity, and the number of iterations
@@ -1758,8 +1760,9 @@ void Meca::apply()
         #pragma omp parallel for num_threads(NUM_THREADS)
         for ( Mecable * mec : mecables )
         {
-            mec->getForces(vFOR+DIM*mec->matIndex());
-            mec->getPoints(vPTS+DIM*mec->matIndex());
+            size_t off = DIM * mec->matIndex();
+            mec->getPoints(vPTS+off);
+            mec->getForces(vFOR+off);
         }
     }
     else

src/sim/mecafil.cc
@@ -192,18 +192,27 @@ void Mecafil::projectForces(const real* X, real* Y) const
 }
 
 void Mecafil::computeTensions(const real*) {} //DIM == 1
-void Mecafil::storeTensions(const real*) {} //DIM == 1
 void Mecafil::makeProjectionDiff(const real*) {} //DIM == 1
 void Mecafil::addProjectionDiff(const real*, real*) const {} //DIM == 1
 
 #endif
 
 
-void Mecafil::printTensions(std::ostream& os) const
+void Mecafil::printTensions(FILE * out, char c) const
 {
-    os << "\n" << reference() << " ";
-    VecPrint::print(os, nbSegments(), iLag, 2);
-    os << " end " << std::fixed << std::setprecision(2) << netForceEndM() << "   " << netForceEndP();
+    fprintf(out, "\n%c%s ", c, reference().c_str());
+    VecPrint::print(out, nbSegments(), iLag, 2);
+    fprintf(out, "  fM"); netForceEndM().print(out);
+    fprintf(out, "  fP"); netForceEndP().print(out);
+}
+
+
+void Mecafil::getForces(const real* ptr)
+{
+    Mecable::getForces(ptr);
+    //fprintf(stderr, "\nF "); VecPrint::print(stderr, DIM*nbPoints(), ptr, 2, DIM);
+    computeTensions(ptr);
+    //printTensions(stderr);
 }
 
 //-----------------------------------------------------------------------

src/sim/mecafil_project.cc
@@ -305,17 +305,6 @@ void Mecafil::computeTensions(const real* force)
 }
 
 
-void Mecafil::storeTensions(const real* force)
-{
-#if NEW_SKIP_PROJECTION
-    if ( skipProjection )
-        computeTensions(force);
-    else
-#endif
-        copy_real(nPoints, iLLG, iLag);
-}
-
-
 void Mecafil::printProjection(std::ostream& os) const
 {
     const size_t nbv = DIM * nbPoints();
@@ -342,32 +331,41 @@ void Mecafil::printProjection(std::ostream& os) const
 
 //------------------------------------------------------------------------------
 #pragma mark - Projection DIFF
-
+//#include "cytoblas.h"
 
 void Mecafil::makeProjectionDiff(const real* force)
 {
     const size_t nbs = nbSegments();
     assert_true( nbs > 0 );
     
-#if ( 0 )
-    // verify that we have the correct Lagrange multipliers:
-    copy_real(nbs, iLag, iLLG);
+#if NEW_SKIP_PROJECTION
+    if ( skipProjection )
+    {
+        useProjectionDiff = false;
+        return;
+    }
+#endif
+
+#if 0
+    // Check here that iLLG[] contains the correct Lagrange multipliers
+    // compute Lagrange multipliers corresponding to 'force' in iLag:
     computeTensions(force);
     real n = blas::max_diff(nbs, iLLG, iLag);
     if ( n > 1e-6 )
     {
-        std::clog << "Error= \n" << n << "\n";
-        std::clog << "Lagrange: "; VecPrint::print(std::clog, std::min(20u,nbs), iLLG);
-        std::clog << "Multipl.: "; VecPrint::print(std::clog, std::min(20u,nbs), iLag);
-        std::clog << "\n";
+        fprintf(stderr, "\n|iLag - iLLG| = %e", n);
+        fprintf(stderr, "\niLag "); VecPrint::print(std::clog, std::min(20LU,nbs), iLag);
+        fprintf(stderr, "\niLLG "); VecPrint::print(std::clog, std::min(20LU,nbs), iLLG);
     }
 #endif
     
-    //----- we remove compressive forces ( negative Lagrange-multipliers )
+    // use Lagrange multipliers computed from the last projectForces() in iLLG
+
+    // remove compressive forces ( negative Lagrange-multipliers )
     useProjectionDiff = false;
     for ( size_t jj = 0; jj < nbs; ++jj )
     {
-        if ( iLag[jj] > 0 )
+        if ( iLLG[jj] > 0 )
         {
             useProjectionDiff = true;
             break;
@@ -380,7 +378,7 @@ void Mecafil::makeProjectionDiff(const real* force)
         const real sc = 1.0 / segmentation();
         #pragma vector unaligned
         for ( size_t jj = 0; jj < nbs; ++jj )
-            iJJtiJforce[jj] = std::max(th, iLag[jj] * sc);
+            iJJtiJforce[jj] = std::max(th, iLLG[jj] * sc);
         
         //std::clog << "projectionDiff: " << blas::nrm2(nbs, iJJtiJforce) << std::endl;
         //std::clog << "projectionDiff:"; VecPrint::print(std::clog, std::min(20u,nbs), iJJtiJforce);

andresbonil/cytosim

src/sim/meca.cc
@@ -1144,34 +1144,20 @@ void Meca::prepareMatrices()
  Calculates forces due to external links, without adding Thermal motion,
  and also excluding bending elasticity of Fibers.
  
- This also sets the Lagrange multipliers for the Fiber.
+ Mecable::getForces will also sets the Lagrange multipliers for the Fiber.
  
- The function will not change the position of the Mecables.
+ The function will not change the positions of any Mecable.
  */
 void Meca::computeForces()
 {
     prepareMatrices();
     
-    // calculate forces in vFOR, but without Brownian noise:
+    // vFOR <- external forces
     calculateForces(vPTS, vBAS, vFOR);
-    copy_real(dimension(), vFOR, vTMP);
 
-    // add rigidity, and calculate the Lagrange Multiplier with this:
     for ( Mecable * mec : objs )
     {
-        real * fff = vFOR + DIM * mec->matIndex();
-        real * ttt = vTMP + DIM * mec->matIndex();
-        
-        // add bending rigidity:
-#if ( DIM > 1 )
-        real * xxx = vPTS + DIM * mec->matIndex();
-        mec->addRigidity(xxx, ttt);
-#endif
-        // calculate Lagrange multipliers for Fibers (irrelevant for other object)
-        mec->projectForces(ttt, ttt);
-        mec->storeTensions(ttt);
-        // register force (all objects)
-        mec->getForces(fff);
+        mec->getForces(vFOR+DIM*mec->matIndex());
     }
 }
 
@@ -1179,8 +1165,8 @@ void Meca::computeForces()
 /**
 This preforms:
  
-    vFOR <- vFOR + Noise
-    vRHS <- time_step * P * vFOR:
+    fff <- fff + Brownian
+    rhs <- time_step * P * fff:
  
  Also prepare Projection diff is requested
 
@@ -1193,15 +1179,16 @@ real brownian1(Mecable* mec, real const* rnd, real alpha, real* fff, real beta, 
     // Calculate the right-hand-side of the system in vRHS:
     mec->projectForces(fff, rhs);
     
-    // rhs <- beta * rhs, resulting in time_step * P * vFOR:
+    // rhs <- beta * rhs, resulting in time_step * P * fff:
     blas::xscal(DIM*mec->nbPoints(), beta*mec->leftoverMobility(), rhs, 1);
 
     /*
-     In this case, `fff` contains the true force in each vertex of the system
-     and the Lagrange multipliers will represent the tension in the filaments
+     At this stage, `fff` contains the external forces in each vertex but also
+     internal force such as bending elasticity terms, and the Lagrange multipliers
+     do not represent the true tension in the filaments.
+     Hence we do not call 'computeTensions(fff)' here
      */
-    mec->storeTensions(fff);
-    
+
 #if ADD_PROJECTION_DIFF
     mec->makeProjectionDiff(fff);
 #endif
@@ -1225,27 +1212,35 @@ real brownian1(Mecable* mec, real const* rnd, real alpha, real* fff, real beta, 
  
  where M is a matrix and B is a vector, leading to:
  
-     ( I - time_step * P * M ) ( Xnew - Xold ) = time_step * P * F + Noise
+     ( I - time_step * P * M ) ( Xnew - Xold ) = time_step * P * Force + Noise
  
  where:
  
-     F = M * Xold + B
+     Force = M * Xold + B
  
      Noise = sqrt(2*kT*time_step*mobility) * Gaussian(0,1)
  
  Implicit integration ensures numerical stability. In the code,
- the matrix M is decomposed as:
+ the sparse matrix M is decomposed as:
  
-     M = mB + mC + the rigidity terms of Mecables
+     M = mB + mC + Rigidity_of_Mecables
  
- and the vectors are:
+ Where mB is isotropic: it applies similarly in the X, Y and Z subspaces, and
+ has no crossterms between X and Y or X and Z or Y ans Z subspaces.
+ All crossterms go into mC.
+ The terms should already be set:
+
+     'mB', 'mC' and 'B=vBAS' are set in Meca::setAllInteractions()
+     'vPTS = Xold' is set from Mecables' points in Meca::prepare()
+     
+ The outline of the calculation is:
  
-     'vPTS' is Xold
-     'vBAS' is B
-     'vRND' is Noise, a vector of calibrated Brownian terms
-     'vFOR' is the force ( M * Xold + B ) and then ( M * Xnew + B )
-     'vRHS' is the right-hand-side of the system (time_step * P * F + vRND)
-     'vSOL' is `Xnew - Xold`, the solution to the system
+     'vRND' <- calibrated Gaussian random terms ~N(0,1)
+     'vFOR' <- force 'M * Xold + B'
+     'vRHS' <- time_step * P * F + vRND (right-hand-side of the system)
+     'vSOL' <- solution to the linear system of equations is calculated
+     'vSOL' <- 'Xnew = Xold + vSOL'
+     'vFOR' <- force 'M * Xnew + B'
  
  */
 void Meca::solve(SimulProp const* prop, const int precond)
@@ -1256,7 +1251,7 @@ void Meca::solve(SimulProp const* prop, const int precond)
 
     prepareMatrices();
     
-    // calculate forces before constraints in vFOR:
+    // calculate external forces in vFOR:
     calculateForces(vPTS, vBAS, vFOR);
     
 #if ( DIM > 1 )
@@ -1493,16 +1488,24 @@ void Meca::solve(SimulProp const* prop, const int precond)
     
 #endif
     
-    //add the solution of the system (=dPTS) to the points coordinates
+    //add the solution (the displacement) to update the Mecable's vertices
     blas::add(dimension(), vSOL, vPTS);
     
     /*
-     Re-calculate forces with the new coordinates, excluding bending elasticity
-     and Brownian terms on the vertices.
-     In this way the result returned to the fibers does not sum-up to zero,
-     and is appropriate for example to calculate the effect of force on assembly.
+     Re-calculate forces with the new coordinates, excluding bending elasticity.
+     In this way the forces returned to the fibers do not sum-up to zero, and
+     are appropriate for example to calculate the effect of force on assembly.
      */
     calculateForces(vPTS, vBAS, vFOR);
+    
+    // add Brownian terms:
+    for ( Mecable * mec : objs )
+    {
+        const size_t inx = DIM * mec->matIndex();
+        mec->addBrownianForces(vRND+inx, alpha, vFOR+inx);
+        //fprintf(stderr, "\n  "); VecPrint::print(stderr, DIM*mec->nbPoints(), vFOR+inx, 2, DIM);
+    }
+
     ready_ = 1;
     
     // report on the matrix type and size, sparsity, and the number of iterations
@@ -1544,8 +1547,9 @@ void Meca::apply()
 #else
         for ( Mecable * mec : objs )
         {
-            mec->getForces(vFOR+DIM*mec->matIndex());
-            mec->getPoints(vPTS+DIM*mec->matIndex());
+            size_t off = DIM * mec->matIndex();
+            mec->getPoints(vPTS+off);
+            mec->getForces(vFOR+off);
         }
 #endif
     }

src/sim/mecafil.cc
@@ -151,18 +151,27 @@ void Mecafil::projectForces(const real* X, real* Y) const
 }
 
 void Mecafil::computeTensions(const real*) {} //DIM == 1
-void Mecafil::storeTensions(const real*) {} //DIM == 1
 void Mecafil::makeProjectionDiff(const real*) {} //DIM == 1
 void Mecafil::addProjectionDiff(const real*, real*) const {} //DIM == 1
 
 #endif
 
 
-void Mecafil::printTensions(std::ostream& os) const
+void Mecafil::printTensions(FILE * out, char c) const
 {
-    os << "\n" << reference() << " ";
-    VecPrint::print(os, nbSegments(), rfLag, 2);
-    os << " end " << std::fixed << std::setprecision(2) << netForceEndM() << "   " << netForceEndP();
+    fprintf(out, "\n%c%s ", c, reference().c_str());
+    VecPrint::print(out, nbSegments(), rfLag, 2);
+    fprintf(out, "  fM"); netForceEndM().print(out);
+    fprintf(out, "  fP"); netForceEndP().print(out);
+}
+
+
+void Mecafil::getForces(const real* ptr)
+{
+    Mecable::getForces(ptr);
+    //fprintf(stderr, "\nF "); VecPrint::print(stderr, DIM*nbPoints(), ptr, 2, DIM);
+    computeTensions(ptr);
+    //printTensions(stderr);
 }
 
 //-----------------------------------------------------------------------

src/sim/mecafil_project.cc
@@ -545,12 +545,6 @@ void Mecafil::computeTensions(const real* force)
 }
 
 
-void Mecafil::storeTensions(const real*)
-{
-    copy_real(nPoints, rfLLG, rfLag);
-}
-
-
 void Mecafil::printProjection(std::ostream& os) const
 {
     const unsigned nbv = DIM * nbPoints();
@@ -577,32 +571,33 @@ void Mecafil::printProjection(std::ostream& os) const
 
 //------------------------------------------------------------------------------
 #pragma mark - Projection DIFF
-
+//#include "cytoblas.h"
 
 void Mecafil::makeProjectionDiff(const real* force)
 {
     const unsigned nbs = nbSegments();
     assert_true( nbs > 0 );
     
-#if ( 0 )
-    // verify that we have the correct Lagrange multipliers:
-    copy_real(nbs, rfLag, rfLLG);
+#if 0
+    // Check here that iLLG[] contains the correct Lagrange multipliers
+    // compute Lagrange multipliers corresponding to 'force' in iLag:
     computeTensions(force);
     real n = blas::max_diff(nbs, rfLLG, rfLag);
     if ( n > 1e-6 )
     {
-        std::clog << "Error= \n" << n << "\n";
-        std::clog << "Lagrange: "; VecPrint::print(std::clog, std::min(20u,nbs), rfLLG);
-        std::clog << "Multipl.: "; VecPrint::print(std::clog, std::min(20u,nbs), rfLag);
-        std::clog << "\n";
+        fprintf(stderr, "\n|iLag - iLLG| = %e", n);
+        fprintf(stderr, "\niLag "); VecPrint::print(std::clog, std::min(20LU,nbs), rfLag);
+        fprintf(stderr, "\niLLG "); VecPrint::print(std::clog, std::min(20LU,nbs), rfLLG);
     }
 #endif
     
-    //----- we remove compressive forces ( negative Lagrange-multipliers )
+    // use Lagrange multipliers computed from the last projectForces() in iLLG
+
+    // remove compressive forces ( negative Lagrange-multipliers )
     useProjectionDiff = false;
     for ( unsigned jj = 0; jj < nbs; ++jj )
     {
-        if ( rfLag[jj] > 0 )
+        if ( rfLLG[jj] > 0 )
         {
             useProjectionDiff = true;
             break;
@@ -614,8 +609,8 @@ void Mecafil::makeProjectionDiff(const real* force)
         const real th = 0.0;
         const real sc = 1.0 / segmentation();
         #pragma vector unaligned
-        for ( unsigned jj = 0; jj < nbs; ++jj )
-            mtJJtiJforce[jj] = std::max(th, rfLag[jj] * sc);
+        for ( size_t jj = 0; jj < nbs; ++jj )
+            mtJJtiJforce[jj] = std::max(th, rfLLG[jj] * sc);
         
         //std::clog << "projectionDiff: " << blas::nrm2(nbs, mtJJtiJforce) << std::endl;
         //std::clog << "projectionDiff:"; VecPrint::print(std::clog, std::min(20u,nbs), mtJJtiJforce);