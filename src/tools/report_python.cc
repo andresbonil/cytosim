71c2c437b11ae0d7fe83ff0f33b5e0c5dea0ceb7
SergeDmi <serge.dmitrieff@gmail.com>
2022-03-23 18:06:30 +0100

    minor cleanup

--- src/tools/report_python.cc
+++ src/tools/report_python.cc
@@ -22,8 +22,7 @@
 #include "objecter_python.h"
 
 namespace py = pybind11;
-int verbose = 1;
-int prefix = 0;
+
 size_t cnt = 0;
 Simul simul;
 FrameReader reader;
@@ -40,7 +39,9 @@ typedef std::unordered_map<std::string,std::string> prop_strings;
 typedef std::unordered_map<std::string,PyObj> map_objs;
 
 int load_simul()
-{
+{   
+    int verbose = 1;
+    int prefix = 0;
     
     Glossary arg;
 
@@ -102,75 +103,12 @@ pyarray report_loaded_frame(int fr)
     return pypts;
 }
 
-void change_status(int s) {
-    status = s;
-}
 
 int get_status() {
     return status;
 }
-// Just returns a PyObj, i.e. a struct with a py::array and a real
-PyObj report_fib_frame(int fr)
-{
-    
-    Glossary arg;
 
-    std::string input = TRAJECTORY;
-    std::string str;
 
-    unsigned frame = fr;
-    unsigned period = 1;
-
-    arg.set(input, ".cmo") || arg.set(input, "input");
-    arg.set(verbose, "verbose");
-    if ( arg.use_key("-") ) verbose = 0;
-
-    Simul simul;
-    FrameReader reader;
-
-    try
-    {
-        RNG.seed();
-        simul.loadProperties();
-        reader.openFile(input);
-        Cytosim::all_silent();
-
-        reader.loadFrame(simul, frame);
-    }
-    catch( Exception & e )
-    {
-        std::clog << "Aborted: " << e.what() << '\n';
-        //return EXIT_FAILURE;
-    }
-
-    PyObj pobj(simul.fibers.firstID());
-
-    return pobj;
-}
-
-// Simplest example
-int add(int i, int j) {
-    return i + j;
-}
-
-// reports a py::array directy
-pyarray ff_report(int i) {
-    // Returns a numpy array for the position of the first point of the fiber at frame i
-    pyarray arr = report_loaded_frame(i);
-    return arr;
-}
-
-// returns a py_obj
-PyObj get_frame_obj(int i) {
-    PyObj fib = report_fib_frame(i);
-    return fib;
-}
-
-// returns a vector of PyObj
-obj_vec get_frames(int i, int j) {
-    obj_vec fibs{get_frame_obj(i), get_frame_obj(j) };
-    return fibs;
-}
 
 /// Showcasing making dictionaries
 py::dict get_props() {
@@ -184,10 +122,10 @@ py::dict get_props() {
 }
 
 /// returns an unordered map of PyObj
-map_objs get_objs(int i, int j) {
-    map_objs objs{{std::to_string(i),get_frame_obj(i)},{std::to_string(j),get_frame_obj(j)}};
-    return objs;
-}
+//map_objs get_objs(int i, int j) {
+//    map_objs objs{{std::to_string(i),get_frame_obj(i)},{std::to_string(j),get_frame_obj(j)}};
+ //   return objs;
+//}
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
@@ -196,13 +134,8 @@ PYBIND11_MODULE(example, m) {
         //.def_readwrite("pos0", &PyObj::position);
     a.def_readwrite("pos0", &PyObj::position);
 
-    m.def("add", &add, "A function that adds two numbers");
-    m.def("report", &ff_report, "A function that reports fiber frame f");
-    m.def("getobj", &get_frame_obj, "A function that reports fiber frame f");
-    m.def("get_two", &get_frames, "A function that reports fiber frame f");
+    
     m.def("get_reals", &get_props, "A function that reports fiber frame f");
-    m.def("get_objs", &get_objs, "A function that reports fiber frame f");
-    m.def("set_status", &change_status, "A function that reports fiber frame f");
     m.def("status", &get_status, "blaaa");
     m.def("report_loaded", &report_loaded_frame, "blaaa");
     m.def("load", &load_simul, "load simulation");
