d47569b1e759ba91eb37ef330be0b9ca1905c901
SergeDmi <serge.dmitrieff@gmail.com>
2022-04-14 10:24:37 +0200

    added space repr

---
+++ src/tools/space_modules.h
@@ -0,0 +1,26 @@
+#include "space.h"
+#include <pybind11/pybind11.h>
+#include "python_utilities.h"
+//#include <pybind11/numpy.h>
+//#include <pybind11/stl.h>
+namespace py = pybind11;
+
+class Space;
+class Object;
+//class Property;
+
+/// a utility to enrich the cytosim python module
+void load_space_classes(py::module_ &m) {
+     /// Python interface to space
+    py::class_<Space,Object>(m, "Space");
+        /**
+         * 
+            @TODO : complete with fiber base functions
+         * 
+        */
+         
+         /**
+            @TODO : ADD SPECIALIZED FIBER CLASSES
+         */
+}
+
