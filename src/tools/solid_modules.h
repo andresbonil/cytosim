4221ac54d4629f4bb58995b761c5dab849cf6864
SergeDmi <serge.dmitrieff@gmail.com>
2022-04-26 14:15:14 +0200

    better support for solid

--- src/tools/solid_modules.h
+++ src/tools/solid_modules.h
@@ -14,9 +14,18 @@ void load_solid_classes(py::module_ &m) {
     py::class_<Solid,Object>(m, "Solid")
         .def("position", [](const Solid * sol) {return to_numpy(sol->position());})
         .def("points",  [](Solid * sol) {return get_obj_points(sol);})
+        .def("dragCoefficient",  [](Solid * sol) {return sol->dragCoefficient() ;})
+        .def("addSphere",  [](Solid * sol, pyarray pts, real radius) {return sol->addSphere(to_vector(pts), radius) ;})
+        .def("setRadius",  [](Solid * sol, int i, real radius) {return sol->radius((unsigned) i, radius) ;})
+        .def("radius",  [](Solid * sol, int i) {return sol->radius((unsigned) i) ;})
+        .def("centroid", [](const Solid * sol) {return to_numpy(sol->centroid());})
+        .def("next",  [](Solid * sol) {return sol->next() ;})
+        .def("prev",  [](Solid * sol) {return sol->prev() ;})
+        .def("tag",  [](Solid * sol) {return sol->tag() ;})
+        .def("property",  [](Solid * sol) {return sol->property() ;})
         .def("nbPoints", [](const Solid * sol) {return sol->nbPoints();});
-        
         /**
+         * 
          * 
             @TODO : complete with fiber base functions
          * 
