7643b21425af62e2537ddcae0e415bfbfd4aeab1
SergeDmi <serge.dmitrieff@gmail.com>
2022-06-29 10:40:17 +0200

    support for spaces

--- src/cpython/space_modules.h
+++ src/cpython/space_modules.h
@@ -8,21 +8,45 @@ namespace py = pybind11;
 
 class Space;
 class Object;
-//class Property;
+/// Converts an object to a space if possible;
+static Space* toSpace(Object * obj)
+{
+    if ( obj  &&  obj->tag() == 'e' )
+        return static_cast<Space*>(obj);
+    return nullptr;
+}
 
 /// a utility to enrich the cytosim python module
 void load_space_classes(py::module_ &m) {
      /// Python interface to space
-    py::class_<Space,Object>(m, "Space");
-        /**
-         * 
-            @TODO : complete with fiber base functions
-         * 
-        */
-         
-         /**
-            @TODO : ADD SPECIALIZED FIBER CLASSES
-         */
+    py::class_<Space,Object>(m, "Space")
+        .def("thickness", &Space::thickness)
+        .def("resize",  [](Space * sp, std::string sizes) {
+            Glossary glos = Glossary(sizes); sp->resize(glos) ;})
+        .def("volume", &Space::volume)
+        .def("boundaries",  [](const Space * sp)
+            {Vector V,W; 
+            sp->boundaries(V,W);
+                return std::vector<pyarray>{to_numpy(V),to_numpy(W)};
+                })
+        .def("inside", [](const Space * sp, pyarray pos) {return sp->inside(to_vector(pos));})
+        .def("project", [](const Space * sp, pyarray pos) {return to_numpy(sp->project(to_vector(pos)));})
+        .def("allInside", [](const Space * sp, pyarray pos, real rad) {return sp->allInside(to_vector(pos),rad);})
+        .def("allOutside", [](const Space * sp, pyarray pos, real rad) {return sp->allOutside(to_vector(pos),rad);})
+        .def("toSpace",  [](Object * s) {return toSpace(s);}, py::return_value_policy::reference)
+        .def("max_extension", &Space::max_extension)
+        .def("projectDeflated", [](const Space * sp, pyarray pos, real rad) 
+            {return to_numpy(sp->projectDeflated(to_vector(pos),rad));})
+        .def("distanceToEdgeSqr", [](const Space * sp, pyarray pos) {return sp->distanceToEdgeSqr(to_vector(pos));})
+        .def("distanceToEdge", [](const Space * sp, pyarray pos) {return sp->distanceToEdge(to_vector(pos));})
+        .def("signedDistanceToEdge", [](const Space * sp, pyarray pos) {return sp->signedDistanceToEdge(to_vector(pos));})
+        .def("bounce", [](const Space * sp, pyarray pos) {return to_numpy(sp->bounce(to_vector(pos)));})
+        .def("normalToEdge", [](const Space * sp, pyarray pos) {return to_numpy(sp->normalToEdge(to_vector(pos)));})
+        .def("randomPlace", [](const Space * sp) {return to_numpy(sp->randomPlace());})
+        .def("randomPlaceNearEdge", [](const Space * sp, real rad, int tries) {return to_numpy(sp->randomPlaceNearEdge(rad,tries));})
+        .def("randomPlaceOnEdge", [](const Space * sp, real rad, int tries) {return to_numpy(sp->randomPlaceNearEdge(rad,tries));})
+        .def("estimateVolume", &Space::estimateVolume);
+
     py::class_<SpaceProp,Property>(m, "SpaceProp");
             
     py::enum_<Confinement>(m,"Confinement")
